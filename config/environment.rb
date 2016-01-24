5741ec3b3311c6f981c520d18726598c03230f5d
Alan Koger <adkoge@gmail.com>
2016-01-24 19:27:05 +0000

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Quiz2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiz2::Application.config.secret_key_base = '0af947dc86f43b6b333ae986fbd4729821feabe0fa3a991c27569f2dd5f3e70344e892ff7fda33297570db239704d1bb4e10128fe08cbe397975b2eaba81a1c3'
