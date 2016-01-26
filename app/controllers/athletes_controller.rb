735458c3b61d4599b2e4f8785e2271a1ba641221
Alan Koger <adkoge@gmail.com>
2016-01-26 22:19:39 +0000

    Added user validation rules to submit

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -8,7 +8,10 @@ class AthletesController < ApplicationController
 	end
 
 	def create
-		Athlete.create(quote_params)
+		@athlete = Athlete.create(quote_params)
+		if @athlete.invalid?
+			flash[:error] = 'Could not save, the data you entered is invalid.'
+		end
 		redirect_to root_path
 	end
 
--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,2 +1,6 @@
 class Athlete < ActiveRecord::Base
+	validates :person, :presence => true, :length =>
+	{ :maximum => 160, :minimum => 3 }
+	validates :sport, :presence => true, :length =>
+	{ :maximum => 60, :minimum => 3}
 end
